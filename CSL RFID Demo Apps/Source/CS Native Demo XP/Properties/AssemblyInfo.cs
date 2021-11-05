c1af62f3f0634a94e159d5e7d21dec3e30a401b1
mephist-cne <mephist.siu@cne.com.hk>
2021-11-05 10:26:19 +0800

    3.1.212
    Change Taiwan frequencies

--- CSL RFID Demo Apps/Source/CS Native Demo XP/Properties/AssemblyInfo.cs
+++ CSL RFID Demo Apps/Source/CS Native Demo XP/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.211")]
-[assembly: AssemblyFileVersion("3.1.211")]
+[assembly: AssemblyVersion("3.1.212")]
+[assembly: AssemblyFileVersion("3.1.212")]
