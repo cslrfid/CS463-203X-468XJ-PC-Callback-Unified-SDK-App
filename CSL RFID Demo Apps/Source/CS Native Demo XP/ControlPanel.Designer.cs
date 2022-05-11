46d3a32f6f858c744d52910942df87813064c71b
mephist-cne <mephist.siu@cne.com.hk>
2022-05-11 13:57:10 +0800

    Version 3.1.213
    Fix UI string "dBuV" and set default to "dBmV"

--- CSL RFID Demo Apps/Source/CS Native Demo XP/ControlPanel.Designer.cs
+++ CSL RFID Demo Apps/Source/CS Native Demo XP/ControlPanel.Designer.cs
@@ -78,11 +78,11 @@ namespace CS203_CALLBACK_API_DEMO
             // 
             // toolStripContainer1.ContentPanel
             // 
-            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(125, 780);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(125, 845);
             this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
             this.toolStripContainer1.Name = "toolStripContainer1";
-            this.toolStripContainer1.Size = new System.Drawing.Size(125, 805);
+            this.toolStripContainer1.Size = new System.Drawing.Size(125, 872);
             this.toolStripContainer1.TabIndex = 0;
             this.toolStripContainer1.Text = "toolStripContainer1";
             // 
@@ -114,13 +114,13 @@ namespace CS203_CALLBACK_API_DEMO
             this.toolStripContainer2.ContentPanel.Controls.Add(this.checkBoxReadTID);
             this.toolStripContainer2.ContentPanel.Controls.Add(this.cb_sql);
             this.toolStripContainer2.ContentPanel.Controls.Add(this.toolStrip1);
-            this.toolStripContainer2.ContentPanel.Size = new System.Drawing.Size(125, 805);
+            this.toolStripContainer2.ContentPanel.Size = new System.Drawing.Size(125, 872);
             this.toolStripContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.toolStripContainer2.LeftToolStripPanelVisible = false;
             this.toolStripContainer2.Location = new System.Drawing.Point(0, 0);
             this.toolStripContainer2.Name = "toolStripContainer2";
             this.toolStripContainer2.RightToolStripPanelVisible = false;
-            this.toolStripContainer2.Size = new System.Drawing.Size(125, 805);
+            this.toolStripContainer2.Size = new System.Drawing.Size(125, 872);
             this.toolStripContainer2.TabIndex = 1;
             this.toolStripContainer2.Text = "toolStripContainer2";
             this.toolStripContainer2.TopToolStripPanelVisible = false;
@@ -129,9 +129,9 @@ namespace CS203_CALLBACK_API_DEMO
             // 
             this.groupBox2.Controls.Add(this.radioButton_TXT);
             this.groupBox2.Controls.Add(this.radioButton_CSV);
-            this.groupBox2.Location = new System.Drawing.Point(3, 608);
+            this.groupBox2.Location = new System.Drawing.Point(3, 659);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(114, 45);
+            this.groupBox2.Size = new System.Drawing.Size(114, 49);
             this.groupBox2.TabIndex = 26;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "File format";
@@ -140,9 +140,9 @@ namespace CS203_CALLBACK_API_DEMO
             // 
             this.radioButton_TXT.AutoSize = true;
             this.radioButton_TXT.Checked = true;
-            this.radioButton_TXT.Location = new System.Drawing.Point(58, 21);
+            this.radioButton_TXT.Location = new System.Drawing.Point(58, 23);
             this.radioButton_TXT.Name = "radioButton_TXT";
-            this.radioButton_TXT.Size = new System.Drawing.Size(45, 16);
+            this.radioButton_TXT.Size = new System.Drawing.Size(46, 17);
             this.radioButton_TXT.TabIndex = 18;
             this.radioButton_TXT.TabStop = true;
             this.radioButton_TXT.Text = "TXT";
@@ -151,9 +151,9 @@ namespace CS203_CALLBACK_API_DEMO
             // radioButton_CSV
             // 
             this.radioButton_CSV.AutoSize = true;
-            this.radioButton_CSV.Location = new System.Drawing.Point(9, 21);
+            this.radioButton_CSV.Location = new System.Drawing.Point(9, 23);
             this.radioButton_CSV.Name = "radioButton_CSV";
-            this.radioButton_CSV.Size = new System.Drawing.Size(45, 16);
+            this.radioButton_CSV.Size = new System.Drawing.Size(46, 17);
             this.radioButton_CSV.TabIndex = 17;
             this.radioButton_CSV.Text = "CSV";
             this.radioButton_CSV.UseVisualStyleBackColor = true;
@@ -162,9 +162,9 @@ namespace CS203_CALLBACK_API_DEMO
             // 
             this.groupBox1.Controls.Add(this.radioButton_dBm);
             this.groupBox1.Controls.Add(this.radioButton_dBuV);
-            this.groupBox1.Location = new System.Drawing.Point(0, 489);
+            this.groupBox1.Location = new System.Drawing.Point(0, 530);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(114, 46);
+            this.groupBox1.Size = new System.Drawing.Size(114, 50);
             this.groupBox1.TabIndex = 25;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "RSSI";
@@ -172,23 +172,23 @@ namespace CS203_CALLBACK_API_DEMO
             // radioButton_dBm
             // 
             this.radioButton_dBm.AutoSize = true;
-            this.radioButton_dBm.Location = new System.Drawing.Point(62, 21);
+            this.radioButton_dBm.Checked = true;
+            this.radioButton_dBm.Location = new System.Drawing.Point(62, 23);
             this.radioButton_dBm.Name = "radioButton_dBm";
-            this.radioButton_dBm.Size = new System.Drawing.Size(46, 16);
+            this.radioButton_dBm.Size = new System.Drawing.Size(46, 17);
             this.radioButton_dBm.TabIndex = 1;
+            this.radioButton_dBm.TabStop = true;
             this.radioButton_dBm.Text = "dBm";
             this.radioButton_dBm.UseVisualStyleBackColor = true;
             // 
             // radioButton_dBuV
             // 
             this.radioButton_dBuV.AutoSize = true;
-            this.radioButton_dBuV.Checked = true;
-            this.radioButton_dBuV.Location = new System.Drawing.Point(9, 21);
+            this.radioButton_dBuV.Location = new System.Drawing.Point(9, 23);
             this.radioButton_dBuV.Name = "radioButton_dBuV";
-            this.radioButton_dBuV.Size = new System.Drawing.Size(57, 16);
+            this.radioButton_dBuV.Size = new System.Drawing.Size(51, 17);
             this.radioButton_dBuV.TabIndex = 0;
-            this.radioButton_dBuV.TabStop = true;
-            this.radioButton_dBuV.Text = "dB£gV";
+            this.radioButton_dBuV.Text = "dBuV";
             this.radioButton_dBuV.UseVisualStyleBackColor = true;
             // 
             // checkBox_ShowWindows
@@ -196,50 +196,50 @@ namespace CS203_CALLBACK_API_DEMO
             this.checkBox_ShowWindows.AutoSize = true;
             this.checkBox_ShowWindows.Checked = true;
             this.checkBox_ShowWindows.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox_ShowWindows.Location = new System.Drawing.Point(12, 239);
+            this.checkBox_ShowWindows.Location = new System.Drawing.Point(12, 259);
             this.checkBox_ShowWindows.Name = "checkBox_ShowWindows";
-            this.checkBox_ShowWindows.Size = new System.Drawing.Size(97, 16);
+            this.checkBox_ShowWindows.Size = new System.Drawing.Size(100, 17);
             this.checkBox_ShowWindows.TabIndex = 24;
             this.checkBox_ShowWindows.Text = "Show Windows";
             this.checkBox_ShowWindows.UseVisualStyleBackColor = true;
             // 
             // textBox_RetryCount
             // 
-            this.textBox_RetryCount.Location = new System.Drawing.Point(75, 580);
+            this.textBox_RetryCount.Location = new System.Drawing.Point(75, 628);
             this.textBox_RetryCount.Name = "textBox_RetryCount";
-            this.textBox_RetryCount.Size = new System.Drawing.Size(31, 22);
+            this.textBox_RetryCount.Size = new System.Drawing.Size(31, 20);
             this.textBox_RetryCount.TabIndex = 23;
             this.textBox_RetryCount.Text = "7";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1, 583);
+            this.label2.Location = new System.Drawing.Point(1, 632);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 12);
+            this.label2.Size = new System.Drawing.Size(63, 13);
             this.label2.TabIndex = 22;
             this.label2.Text = "Retry Count";
             // 
             // textBox_LogBufferSize
             // 
-            this.textBox_LogBufferSize.Location = new System.Drawing.Point(90, 287);
+            this.textBox_LogBufferSize.Location = new System.Drawing.Point(90, 311);
             this.textBox_LogBufferSize.Name = "textBox_LogBufferSize";
-            this.textBox_LogBufferSize.Size = new System.Drawing.Size(28, 22);
+            this.textBox_LogBufferSize.Size = new System.Drawing.Size(28, 20);
             this.textBox_LogBufferSize.TabIndex = 21;
             this.textBox_LogBufferSize.Text = "10";
             // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(27, 257);
+            this.label5.Location = new System.Drawing.Point(27, 278);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(92, 29);
+            this.label5.Size = new System.Drawing.Size(92, 31);
             this.label5.TabIndex = 20;
             this.label5.Text = "Group tags from different ants";
             // 
             // checkBox_GroupTags
             // 
             this.checkBox_GroupTags.AutoSize = true;
-            this.checkBox_GroupTags.Location = new System.Drawing.Point(12, 240);
+            this.checkBox_GroupTags.Location = new System.Drawing.Point(12, 260);
             this.checkBox_GroupTags.Name = "checkBox_GroupTags";
             this.checkBox_GroupTags.Size = new System.Drawing.Size(15, 14);
             this.checkBox_GroupTags.TabIndex = 19;
@@ -248,43 +248,43 @@ namespace CS203_CALLBACK_API_DEMO
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 659);
+            this.label3.Location = new System.Drawing.Point(15, 714);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 12);
+            this.label3.Size = new System.Drawing.Size(69, 13);
             this.label3.TabIndex = 15;
             this.label3.Text = "Log File Path";
             // 
             // textBox_LogPath
             // 
-            this.textBox_LogPath.Location = new System.Drawing.Point(1, 674);
+            this.textBox_LogPath.Location = new System.Drawing.Point(1, 730);
             this.textBox_LogPath.Multiline = true;
             this.textBox_LogPath.Name = "textBox_LogPath";
-            this.textBox_LogPath.Size = new System.Drawing.Size(116, 51);
+            this.textBox_LogPath.Size = new System.Drawing.Size(116, 55);
             this.textBox_LogPath.TabIndex = 14;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1, 559);
+            this.label1.Location = new System.Drawing.Point(1, 606);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 12);
+            this.label1.Size = new System.Drawing.Size(68, 13);
             this.label1.TabIndex = 11;
             this.label1.Text = "FileSize (MB)";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(75, 556);
+            this.textBox1.Location = new System.Drawing.Point(75, 602);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(31, 22);
+            this.textBox1.Size = new System.Drawing.Size(31, 20);
             this.textBox1.TabIndex = 10;
             this.textBox1.Text = "5";
             // 
             // checkBoxTagLog
             // 
             this.checkBoxTagLog.AutoSize = true;
-            this.checkBoxTagLog.Location = new System.Drawing.Point(10, 541);
+            this.checkBoxTagLog.Location = new System.Drawing.Point(10, 586);
             this.checkBoxTagLog.Name = "checkBoxTagLog";
-            this.checkBoxTagLog.Size = new System.Drawing.Size(89, 16);
+            this.checkBoxTagLog.Size = new System.Drawing.Size(94, 17);
             this.checkBoxTagLog.TabIndex = 9;
             this.checkBoxTagLog.Text = "Save Tag Log";
             this.checkBoxTagLog.UseVisualStyleBackColor = true;
@@ -292,51 +292,51 @@ namespace CS203_CALLBACK_API_DEMO
             // checkBoxLog
             // 
             this.checkBoxLog.AutoSize = true;
-            this.checkBoxLog.Location = new System.Drawing.Point(12, 289);
+            this.checkBoxLog.Location = new System.Drawing.Point(12, 313);
             this.checkBoxLog.Name = "checkBoxLog";
-            this.checkBoxLog.Size = new System.Drawing.Size(68, 16);
+            this.checkBoxLog.Size = new System.Drawing.Size(72, 17);
             this.checkBoxLog.TabIndex = 8;
             this.checkBoxLog.Text = "Save Log";
             this.checkBoxLog.UseVisualStyleBackColor = true;
             // 
             // textBoxUsercount
             // 
-            this.textBoxUsercount.Location = new System.Drawing.Point(27, 461);
+            this.textBoxUsercount.Location = new System.Drawing.Point(27, 499);
             this.textBoxUsercount.Name = "textBoxUsercount";
-            this.textBoxUsercount.Size = new System.Drawing.Size(70, 22);
+            this.textBoxUsercount.Size = new System.Drawing.Size(70, 20);
             this.textBoxUsercount.TabIndex = 7;
             this.textBoxUsercount.Text = "1";
             // 
             // textBoxUseroffset
             // 
-            this.textBoxUseroffset.Location = new System.Drawing.Point(27, 433);
+            this.textBoxUseroffset.Location = new System.Drawing.Point(27, 469);
             this.textBoxUseroffset.Name = "textBoxUseroffset";
-            this.textBoxUseroffset.Size = new System.Drawing.Size(70, 22);
+            this.textBoxUseroffset.Size = new System.Drawing.Size(70, 20);
             this.textBoxUseroffset.TabIndex = 6;
             this.textBoxUseroffset.Text = "0";
             // 
             // textBoxTIDcount
             // 
-            this.textBoxTIDcount.Location = new System.Drawing.Point(27, 383);
+            this.textBoxTIDcount.Location = new System.Drawing.Point(27, 415);
             this.textBoxTIDcount.Name = "textBoxTIDcount";
-            this.textBoxTIDcount.Size = new System.Drawing.Size(70, 22);
+            this.textBoxTIDcount.Size = new System.Drawing.Size(70, 20);
             this.textBoxTIDcount.TabIndex = 5;
             this.textBoxTIDcount.Text = "1";
             // 
             // textBoxTIDoffset
             // 
-            this.textBoxTIDoffset.Location = new System.Drawing.Point(27, 355);
+            this.textBoxTIDoffset.Location = new System.Drawing.Point(27, 385);
             this.textBoxTIDoffset.Name = "textBoxTIDoffset";
-            this.textBoxTIDoffset.Size = new System.Drawing.Size(70, 22);
+            this.textBoxTIDoffset.Size = new System.Drawing.Size(70, 20);
             this.textBoxTIDoffset.TabIndex = 4;
             this.textBoxTIDoffset.Text = "0";
             // 
             // checkBoxReadUser
             // 
             this.checkBoxReadUser.AutoSize = true;
-            this.checkBoxReadUser.Location = new System.Drawing.Point(10, 411);
+            this.checkBoxReadUser.Location = new System.Drawing.Point(10, 445);
             this.checkBoxReadUser.Name = "checkBoxReadUser";
-            this.checkBoxReadUser.Size = new System.Drawing.Size(72, 16);
+            this.checkBoxReadUser.Size = new System.Drawing.Size(77, 17);
             this.checkBoxReadUser.TabIndex = 3;
             this.checkBoxReadUser.Text = "Read User";
             this.checkBoxReadUser.UseVisualStyleBackColor = true;
@@ -344,9 +344,9 @@ namespace CS203_CALLBACK_API_DEMO
             // checkBoxReadTID
             // 
             this.checkBoxReadTID.AutoSize = true;
-            this.checkBoxReadTID.Location = new System.Drawing.Point(10, 334);
+            this.checkBoxReadTID.Location = new System.Drawing.Point(10, 362);
             this.checkBoxReadTID.Name = "checkBoxReadTID";
-            this.checkBoxReadTID.Size = new System.Drawing.Size(70, 16);
+            this.checkBoxReadTID.Size = new System.Drawing.Size(73, 17);
             this.checkBoxReadTID.TabIndex = 2;
             this.checkBoxReadTID.Text = "Read TID";
             this.checkBoxReadTID.UseVisualStyleBackColor = true;
@@ -354,9 +354,9 @@ namespace CS203_CALLBACK_API_DEMO
             // cb_sql
             // 
             this.cb_sql.AutoSize = true;
-            this.cb_sql.Location = new System.Drawing.Point(12, 311);
+            this.cb_sql.Location = new System.Drawing.Point(12, 337);
             this.cb_sql.Name = "cb_sql";
-            this.cb_sql.Size = new System.Drawing.Size(82, 16);
+            this.cb_sql.Size = new System.Drawing.Size(87, 17);
             this.cb_sql.TabIndex = 1;
             this.cb_sql.Text = "Save to SQL";
             this.cb_sql.UseVisualStyleBackColor = true;
@@ -379,7 +379,7 @@ namespace CS203_CALLBACK_API_DEMO
             this.toolStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.VerticalStackWithOverflow;
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(125, 805);
+            this.toolStrip1.Size = new System.Drawing.Size(125, 872);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -497,9 +497,9 @@ namespace CS203_CALLBACK_API_DEMO
             // 
             // ControlPanelForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(125, 805);
+            this.ClientSize = new System.Drawing.Size(125, 872);
             this.ControlBox = false;
             this.Controls.Add(this.toolStripContainer2);
             this.Controls.Add(this.toolStripContainer1);
@@ -510,8 +510,8 @@ namespace CS203_CALLBACK_API_DEMO
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "Control";
-            this.Load += new System.EventHandler(this.ControlPanelForm_Load);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ControlPanelForm_FormClosing);
+            this.Load += new System.EventHandler(this.ControlPanelForm_Load);
             this.toolStripContainer1.ResumeLayout(false);
             this.toolStripContainer1.PerformLayout();
             this.toolStripContainer2.ContentPanel.ResumeLayout(false);
--- CSL RFID Demo Apps/Source/CS Native Demo XP/Properties/AssemblyInfo.cs
+++ CSL RFID Demo Apps/Source/CS Native Demo XP/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.212")]
-[assembly: AssemblyFileVersion("3.1.212")]
+[assembly: AssemblyVersion("3.1.213")]
+[assembly: AssemblyFileVersion("3.1.213")]
